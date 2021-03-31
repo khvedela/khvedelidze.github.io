451f7f458126d89d40c1640ea87632259f9359db
David khvedelidze <d.khv08@gmail.com>
2021-03-31 22:16:27 +0400

    few major updates

---
+++ scripts/chngTxt.js
@@ -0,0 +1,8 @@
+let text = document.getElementById("text");
+
+let buttonOne = document.getElementById("one");
+let buttonTwo = document.getElementById("two");
+let buttonThree = document.getElementById("three");
+let buttonFour = document.getElementById("four");
+
+onclick
